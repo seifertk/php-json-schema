55a688e282ad3877a46f2f9820929cd7ee2dc29e
Viacheslav Poturaev <nanopeni@gmail.com>
2017-02-22 00:14:52 +0700

    bench

---
+++ tests/src/Benchmarks/SomeBenchmark.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Benchmarks;
+
+
+use Swaggest\JsonSchema\PreProcessor\NameMapper;
+use Swaggest\JsonSchema\Tests\Helper\Order;
+
+class SomeBenchmark
+{
+    public function benchImport()
+    {
+        $mapper = new NameMapper();
+
+        $order = new Order();
+        $order->id = 1;
+        $order->dateTime = '2015-10-28T07:28:00Z';
+        $exported = Order::export($order, $mapper);
+        $imported = Order::import($exported, $mapper);
+    }
+
+}
